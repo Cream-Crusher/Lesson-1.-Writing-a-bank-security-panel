f6021dfcf62789e0ead7508777e7ea881d73cbf9
ruk228 <icecreamcrusher21@gmail.com>
2022-02-12 13:00:15 +0400

    vevel information about the time of visits

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import os
-
+import datetime
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard, Visit  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
     post_passcards = Passcard.objects.all()
     post_passcard = post_passcards[0]
     pass_cards = {
@@ -18,6 +17,17 @@ if __name__ == '__main__':
         'created_at': '{}'.format(post_passcard.created_at),
         'is_active': post_passcard.is_active
     }
-    print(pass_cards)
     active_post_passcards = post_passcards.filter(is_active=True)
-    print(len(active_post_passcards))
+    #print(len(active_post_passcards))
+    #print(pass_cards)
+    #print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    Visits = Visit.objects.all()
+    unclosed_visits = Visits.filter(leaved_at=None)
+
+    for num in range(len(unclosed_visits)):
+
+        entered_at = unclosed_visits[num].entered_at
+        located_in = django.utils.timezone.localtime().replace(microsecond=0) -entered_at
+        passcard = unclosed_visits[num].passcard
+
+        print('{}\nЗашёл в хранилище, время по Москве: {}\nНаходится в хранилище: {}'.format(passcard, entered_at, located_in))
